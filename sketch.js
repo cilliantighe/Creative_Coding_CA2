6d4e4229d4d8dc7cbf1fa45e8f8bac0ee05100a4
N00152737 <N00152737@student.iadt.ie>
2019-02-11 15:19:31 +0000

    V0.1: Canvas

---
+++ sketch.js
@@ -0,0 +1,16 @@
+/*
+Creative Coding
+Author: Cillian Tighe
+Student No: N00152737
+*/
+
+// The 'setup' function is only called once. Everything within the function is executed once
+function setup() {
+  var canvas = createCanvas(windowWidth, windowHeight);
+}
+
+// The 'draw' function is called in a loop. Everything that is in the function is executed continuously
+function draw() {
+  background(255);
+
+}
